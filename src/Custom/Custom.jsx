9f455c1bafe5add37dd4a5f6bd4a8f3b0fd70179
AndresQA <andresquintero199912@gmail.com>
2020-05-15 16:52:23 -0500

    update

--- src/Custom/Custom.jsx
+++ src/Custom/Custom.jsx
@@ -1,22 +1,22 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { Tooltip } from '@material-ui/core';
+import { Tooltip, Button } from '@material-ui/core';
+import ReactAudioPlayer from 'react-audio-player';
+import domtoimage from 'dom-to-image';
+import { saveAs } from 'file-saver';
 
 const Custom = (props) => {
+    
 
+    const [play, setPlay] = React.useState(0);
 
     if (props.location.pathname === "/crea") {
         var background = document.getElementById('background');
         background.style.backgroundImage = 'url(/images/backgroundcustom.jpg)';
     }
 
-    document.addEventListener('DOMContentLoaded', function () {
-
-    });
-
-
-
     const handleClickEars = (index) => {
+        handleFinishSound();
         if (props.ears + index >= 1 && props.ears + index <= 7) {
             props.setEars(props.ears + index);
         } else if (props.ears + index < 1) {
@@ -116,13 +116,10 @@ const Custom = (props) => {
             case 1:
                 let element1 = document.getElementById('mouth')
                 element1.style.top = '';
-                //element1.style.removeProperty();
-
                 break;
             case 2:
                 let element2 = document.getElementById('mouth')
                 element2.style.top = '';
-                //element2.style.removeProperty();
                 break;
 
             case 3:
@@ -133,25 +130,21 @@ const Custom = (props) => {
             case 4:
                 let element4 = document.getElementById('mouth')
                 element4.style.top = '-10px';
-                //element4.style.removeProperty();
                 break;
 
             case 5:
                 let element5 = document.getElementById('mouth')
                 element5.style.top = '';
-                //element5.style.removeProperty();
                 break;
 
             case 6:
                 let element6 = document.getElementById('mouth')
                 element6.style.top = '-20px';
-                //element6.style.removeProperty();
                 break;
 
             case 7:
                 let element7 = document.getElementById('mouth')
                 element7.style.top = '-20px';
-                //element7.style.removeProperty();
                 break;
 
             default:
@@ -206,6 +199,21 @@ const Custom = (props) => {
         }
     }
 
+    const handleFinishSound = () => {
+        setPlay(0);
+    }
+
+    const handleSound = () => {
+        setPlay(1);
+    }
+
+    const handleSave = () => {
+        domtoimage.toBlob(document.getElementById('custom_prevAnimorph'))
+        .then(function (blob) {
+            window.saveAs(blob, 'my-node.png');
+        });
+    }
+
 
 
     return <div className="custom">
@@ -214,7 +222,7 @@ const Custom = (props) => {
                 <img src="./images/Buttons/btnojo.png" alt="" />
             </Tooltip>
             <Tooltip title="Escucha como tu Animorph" arrow placement="left">
-                <img src="./images/Buttons/btnoir.png" alt="" />
+                <img onClick={handleSound} src="./images/Buttons/btnoir.png" alt="" />
             </Tooltip>
         </div>
 
@@ -234,29 +242,44 @@ const Custom = (props) => {
                 </Tooltip>
             </div>
 
-            <div className="custom_prevAnimorph">
+            <div className="custom_prevAnimorph" id="custom_prevAnimorph">
                 <img className="custom_skinPrev" src={'./images/Animorphs/skin' + props.skin + '.png'} alt="" />
                 <img className="custom_chin" id="chin" src={'./images/Animorphs/Chin/barbilla' + props.chin + '.png'} alt="" />
                 <img className="custom_eyes" src={'./images/Animorphs/Eyes/ojos' + props.eyes + '.png'} alt="" />
-                <img className="custom_mouth" id="mouth" src={'./images/Animorphs/Mouth/boca' + props.mouth + '.png'} alt="" />
+                <img className="custom_mouth" id="mouth" src={props.mouth === 1 ? './images/Animorphs/Mouth/boca' + props.mouth + '' + props.skin + '.png' : './images/Animorphs/Mouth/boca' + props.mouth + '.png'} alt="" />
                 <img className="custom_ears" id="ears" src={props.ears === 1 ? './images/Animorphs/Ears/orejas' + props.ears + '' + props.skin + '.png' : './images/Animorphs/Ears/orejas' + props.ears + '.png'} alt="" />
             </div>
 
-            <div className="custom__btnSel right">
+            <div  className="custom__btnSel right">
                 <Tooltip title="Orejas" arrow placement="right">
-                <img src="./images/Buttons/btnflecha.png" onClick={() => handleClickEars(-1)} alt="" />
+                    <img src="./images/Buttons/btnflecha.png" onClick={() => handleClickEars(-1)} alt="" />
                 </Tooltip>
                 <Tooltip title="Ojos" arrow placement="right">
-                <img src="./images/Buttons/btnflecha.png" onClick={() => handleClickEyes(-1)} alt="" />
+                    <img src="./images/Buttons/btnflecha.png" onClick={() => handleClickEyes(-1)} alt="" />
                 </Tooltip>
                 <Tooltip title="Boca" arrow placement="right">
-                <img src="./images/Buttons/btnflecha.png" onClick={() => handleClickMouth(-1)} alt="" />
+                    <img src="./images/Buttons/btnflecha.png" onClick={() => handleClickMouth(-1)} alt="" />
                 </Tooltip>
                 <Tooltip title="Pelaje" arrow placement="right">
-                <img src="./images/Buttons/btnflecha.png" onClick={() => handleClickChin(-1)} alt="" />
+                    <img src="./images/Buttons/btnflecha.png" onClick={() => handleClickChin(-1)} alt="" />
                 </Tooltip>
             </div>
 
+        </div>
+
+        {play === 1 &&
+            <ReactAudioPlayer
+                src={'./audio/' + props.ears + '.mp3'}
+                autoPlay
+                onEnded={handleFinishSound}
+            />
+
+        }
+        {/*
+        <Button variant="contained" color="primary" size="large" onClick={handleSave} >Guardar</Button>
+        */}
+        <div className="buttonposCustom">
+            <Button variant="contained" color="primary" size="large" >Convertir</Button>
         </div>
 
 
