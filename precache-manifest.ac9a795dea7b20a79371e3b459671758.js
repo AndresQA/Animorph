4109992303332d1ff24933920168e979628f2abf
AndresQA <andresquintero199912@gmail.com>
2020-05-15 21:09:19 -0500

    Updates

---
+++ precache-manifest.ac9a795dea7b20a79371e3b459671758.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0b191c0136c7aa29e9069f03dc246c7f",
+    "url": "/Animorph/index.html"
+  },
+  {
+    "revision": "05bc7a3111350a7f95a2",
+    "url": "/Animorph/static/css/main.4ba448b9.chunk.css"
+  },
+  {
+    "revision": "512572a567f8ed072a39",
+    "url": "/Animorph/static/js/2.ae63d8f7.chunk.js"
+  },
+  {
+    "revision": "eac4cf64bf573541fd6dba29c78393a0",
+    "url": "/Animorph/static/js/2.ae63d8f7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "05bc7a3111350a7f95a2",
+    "url": "/Animorph/static/js/main.8449d5d6.chunk.js"
+  },
+  {
+    "revision": "a76a4794e053c9a08987",
+    "url": "/Animorph/static/js/runtime-main.07f8f0fc.js"
+  }
+]);
